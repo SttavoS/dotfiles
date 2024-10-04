76ddc7f82daf51b1502f1bdc0838c8ebb24b0995
Gustavo Schneider <gustavo.schneider.dev@gmail.com>
2024-10-04 01:05:17 -0300

    config lualine and organize plugins

--- nvim/.config/nvim/lua/config/lazy.lua
+++ nvim/.config/nvim/lua/config/lazy.lua
@@ -14,27 +14,3 @@ if not (vim.uv or vim.loop).fs_stat(lazypath) then
   end
 end
 vim.opt.rtp:prepend(lazypath)
-
--- Make sure to setup `mapleader` and `maplocalleader` before
--- loading lazy.nvim so that mappings are correct.
--- This is also a good place to setup other settings (vim.opt)
-vim.g.mapleader = " "
-vim.g.maplocalleader = "\\"
-
--- Setup lazy.nvim
-require("lazy").setup({
-  spec = {
-    -- import your plugins
-    { "ellisonleao/gruvbox.nvim", priority = 1000 , config = true, opts = ...},
-    {
-      'nvim-telescope/telescope.nvim', tag = '0.1.8',
-      dependencies = { 'nvim-lua/plenary.nvim' }
-    },
-    { "nvim-treesitter/nvim-treesitter", build = ":TSUpdate" }
-  },
-  -- Configure any other settings here. See the documentation for more details.
-  -- colorscheme that will be used when installing plugins.
-  install = { colorscheme = { "habamax" } },
-  -- automatically check for plugin updates
-  checker = { enabled = true },
-})
--- nvim/.config/nvim/lua/plugins/treesitter.lua
+++ nvim/.config/nvim/lua/plugins/treesitter.lua
@@ -1,18 +1,13 @@
-local M = {
+return {
   "nvim-treesitter/nvim-treesitter",
-  build = function()
-    require("nvim-treesitter.install").update({ with_sync = true })()
-  end,
+  build = ":TSUpdate",
   config = function()
     local configs = require("nvim-treesitter.configs")
 
     configs.setup({
       ensure_installed = { "lua", "elixir", "javascript", "typescript", "html", "css" },
-      sync_install = false,
       highlight = { enable = true },
       indent = { enable = true },  
     })
   end
 }
-
-return { M }

6f22384d68a5302d93e3aead401255aa0731ce2e
Gustavo Schneider <gustavo.schneider.dev@gmail.com>
2024-10-14 14:32:48 -0300

    nvim: add support to css

--- nvim/.config/nvim/lua/plugins/lsp-config.lua
+++ nvim/.config/nvim/lua/plugins/lsp-config.lua
@@ -24,6 +24,9 @@ return {
 			lspconfig.ts_ls.setup({
 				capabilities = capabilities,
 			})
+      lspconfig.cssls.setup({
+        capabilities = capabilities
+      })
 
 			vim.keymap.set("n", "K", vim.lsp.buf.hover, {})
 			vim.keymap.set("n", "gd", vim.lsp.buf.definition, {})
--- nvim/.config/nvim/lua/plugins/treesitter.lua
+++ nvim/.config/nvim/lua/plugins/treesitter.lua
@@ -1,15 +1,25 @@
 return {
-  {
-    "nvim-treesitter/nvim-treesitter",
-    build = ":TSUpdate",
-    config = function()
-      local configs = require("nvim-treesitter.configs")
+	{
+		"nvim-treesitter/nvim-treesitter",
+		build = ":TSUpdate",
+		config = function()
+			local configs = require("nvim-treesitter.configs")
 
-      configs.setup({
-        ensure_installed = { "lua", "elixir", "javascript", "typescript", "html", "css" },
-        highlight = { enable = true },
-        indent = { enable = true },
-      })
-    end
-  }
+			configs.setup({
+				ensure_installed = {
+					"lua",
+					"javascript",
+					"typescript",
+					"html",
+					"css",
+          "scss",
+          "vue",
+          "rust",
+					"elixir",
+				},
+				highlight = { enable = true },
+				indent = { enable = true },
+			})
+		end,
+	},
 }

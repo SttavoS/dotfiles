934a8fc6df379f56802b941a1e5208389d066277
Gustavo Schneider <gustavo.schneider.dev@gmail.com>
2024-10-20 16:49:15 -0300

    nvim: configure lsp to works correct in vue

--- nvim/.config/nvim/lua/plugins/lsp-config.lua
+++ nvim/.config/nvim/lua/plugins/lsp-config.lua
@@ -15,6 +15,8 @@ return {
 	{
 		"neovim/nvim-lspconfig",
 		config = function()
+      local mason_registry = require('mason-registry')
+      local vue_language_server_path = mason_registry.get_package('vue-language-server'):get_install_path() .. '/node_modules/@vue/language-server'
 			local capabilities = require("cmp_nvim_lsp").default_capabilities()
 			local lspconfig = require("lspconfig")
 
@@ -23,6 +25,16 @@ return {
 			})
 			lspconfig.ts_ls.setup({
 				capabilities = capabilities,
+        init_options = {
+          plugins = {
+            {
+              name = '@vue/typescript-plugin',
+              location = vue_language_server_path,
+              languages = {'vue'}
+            }
+          }
+        },
+        filetypes = { 'typescript', 'javascript', 'javascriptreact', 'typescriptreact', 'vue' }
 			})
       lspconfig.cssls.setup({
         capabilities = capabilities

a45b1131cb21fdccf7515a5d31eb7499b9b5cbcc
Gustavo Schneider <gustavo.schneider.dev@gmail.com>
2024-10-20 02:27:18 -0300

    nvim: add support to vuejs

--- nvim/.config/nvim/lua/plugins/lsp-config.lua
+++ nvim/.config/nvim/lua/plugins/lsp-config.lua
@@ -27,6 +27,9 @@ return {
       lspconfig.cssls.setup({
         capabilities = capabilities
       })
+      lspconfig.volar.setup({
+        capabilities = capabilities
+      })
 
 			vim.keymap.set("n", "K", vim.lsp.buf.hover, {})
 			vim.keymap.set("n", "gd", vim.lsp.buf.definition, {})

3f0efcec75c07b0fdc815a53a41eb62cf7827990
Gustavo Schneider <gustavo.schneider.dev@gmail.com>
2024-11-10 22:06:29 -0300

    nvim: update dashboard

--- nvim/.config/nvim/lua/plugins/dashboard.lua
+++ nvim/.config/nvim/lua/plugins/dashboard.lua
@@ -1,17 +1,34 @@
 return {
-  {
-    "nvimdev/dashboard-nvim",
-    event = "VimEnter",
-    config = function()
-      local headers = require("config.dashboard.headers")
+	{
+		"nvimdev/dashboard-nvim",
+		event = "VimEnter",
+		config = function()
+			local headers = require("config.dashboard.headers")
 
-      require("dashboard").setup({
-        theme = "doom",
-        config = {
-          header = headers.neovim,
-        },
-      })
-    end,
-    dependencies = { { "nvim-tree/nvim-web-devicons" } },
-  },
+			require("dashboard").setup({
+				theme = "doom",
+				config = {
+					header = headers.gundam,
+					center = {
+						{
+							action = 'lua require("telescope.builtin").find_files',
+							desc = " Find Files",
+							icon = "",
+							key = "f",
+						},
+					},
+					footer = function()
+						local stats = require("lazy").stats()
+						local ms = (math.floor(stats.startuptime * 100 + 0.5) / 100)
+						return {
+							"⚡ Neovim loaded " .. stats.loaded .. "/" .. stats.count .. " plugins in " .. ms .. "ms",
+						}
+					end,
+				},
+			})
+
+			vim.api.nvim_set_hl(0, "DashboardHeader", { fg = "#cc241d" })
+		end,
+		dependencies = { { "nvim-tree/nvim-web-devicons" } },
+	},
 }

9ee015ff57e896aaa322fd7014017296b5d7881c
Gustavo Schneider <gustavo.schneider.dev@gmail.com>
2024-11-11 13:26:29 -0300

    nvim: add keybind to toggle Neotree

--- nvim/.config/nvim/lua/plugins/neo-tree.lua
+++ nvim/.config/nvim/lua/plugins/neo-tree.lua
@@ -1,17 +1,22 @@
 return {
-	"nvim-neo-tree/neo-tree.nvim",
-	branch = "v3.x",
-	keys = {
-		{
-			"<leader>e",
-			":Neotree filesystem reveal left<CR>",
-			desc = "Open File Explorer",
-		},
-	},
-	dependencies = {
-		"nvim-lua/plenary.nvim",
-		"nvim-tree/nvim-web-devicons", -- not strictly required, but recommended
-		"MunifTanjim/nui.nvim",
-		"3rd/image.nvim", -- Optional image support in preview window: See `# Preview Mode` for more information
-	},
+  "nvim-neo-tree/neo-tree.nvim",
+  branch = "v3.x",
+  keys = {
+    {
+      "<leader>e",
+      ":Neotree filesystem reveal left<CR>",
+      desc = "Open File Explorer",
+    },
+    {
+      "<leader>et",
+      "<cmd>Neotree toggle<CR>",
+      desc = "Toggle File Explorer",
+    },
+  },
+  dependencies = {
+    "nvim-lua/plenary.nvim",
+    "nvim-tree/nvim-web-devicons", -- not strictly required, but recommended
+    "MunifTanjim/nui.nvim",
+    "3rd/image.nvim",            -- Optional image support in preview window: See `# Preview Mode` for more information
+  },
 }

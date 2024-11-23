7343f65a88c5cf985aab919e5a923035c9f30cc5
Gustavo Schneider <gustavo.schneider.dev@gmail.com>
2024-11-23 14:33:35 -0300

    nvim: change gruvbox theme package

--- nvim/.config/nvim/lua/plugins/colorschemes.lua
+++ nvim/.config/nvim/lua/plugins/colorschemes.lua
@@ -1,32 +1,19 @@
 return {
 	{
-		"sainnhe/gruvbox-material",
-		lazy = false,
+		"ellisonleao/gruvbox.nvim",
 		priority = 1000,
 		config = function()
-			-- Optionally configure and load the colorscheme
-			-- directly inside the plugin declaration.
-			vim.g.gruvbox_material_backgorud = "hard"
-			vim.g.gruvbox_material_foreground = "original"
-			vim.g.gruvbox_material_enable_italic = true
-			vim.cmd.colorscheme("gruvbox-material")
-
-			require("nvim-treesitter.configs").setup({
-				highlight = {
-					enable = true,
-					additional_vim_regex_highlighting = false,
-				},
-			})
-
-			-- Sobrescreva os destaques do Treesitter
-			vim.api.nvim_set_hl(0, "@tag", { fg = "#8ec07c", bg = "NONE", bold = false, italic = false })
-			vim.api.nvim_set_hl(0, "@tag.builtin", { fg = "#8ec07c", bg = "NONE", bold = false, italic = false })
-			vim.api.nvim_set_hl(0, "@tag.attribute", { fg = "#fabd2f", bg = "NONE", bold = false, italic = false })
-			vim.api.nvim_set_hl(0, "@string.javascript", { fg = "#b8bb26", bg = "NONE", bold = false, italic = false })
-			vim.api.nvim_set_hl(0, "@tag.delimiter", { fg = "#83a598", bg = "NONE", bold = false, italic = false })
+			vim.o.background = "dark" -- or "light" for light mode
+			vim.cmd.colorscheme("gruvbox")
 		end,
-	},
-	{
-		"rebelot/kanagawa.nvim",
+		opts = {
+			overrides = {
+				["@tag"] = { fg = "#8ec07c", bg = "NONE", bold = false, italic = false },
+				["@tag.builtin"] = { fg = "#8ec07c", bg = "NONE", bold = false, italic = false },
+				["@tag.attribute"] = { fg = "#fabd2f", bg = "NONE", bold = false, italic = false },
+				["@tag.delimiter"] = { fg = "#83a598", bg = "NONE", bold = false, italic = false },
+				["@string.javascript"] = { fg = "#b8bb26", bg = "NONE", bold = false, italic = false },
+			},
+		},
 	},
 }

fcfc6f71ca96de52090192f9f80f84e9f02acdef
Gustavo Schneider <gustavo.schneider.dev@gmail.com>
2024-11-29 21:43:17 -0300

    nvim: move snacks nvim to ui.lua

--- nvim/.config/nvim/lua/plugins/ui.lua
+++ nvim/.config/nvim/lua/plugins/ui.lua
@@ -7,6 +7,10 @@ return {
 		-- },
 		opts = {
 			options = {
+        -- stylua: ignore
+        close_command = function(n) Snacks.bufdelete(n) end,
+        -- stylua: ignore
+        right_mouse_command = function(n) Snacks.bufdelete(n) end,
 				mode = "buffers",
 				diagnostics = "nvim_lsp",
 				offsets = {
@@ -88,5 +92,46 @@ return {
 			end
 			require("noice").setup(opts)
 		end,
+	},
+	{
+		"folke/snacks.nvim",
+		priority = 1000,
+		lazy = false,
+		opts = {
+			-- your configuration comes here
+			-- or leave it empty to use the default settings
+			-- refer to the configuration section below
+			dashboard = {
+				enabled = true,
+				preset = {
+					header = [[
+███╗   ███╗ █████╗ ████████╗███████╗██╗   ██╗██╗███╗   ███╗
+████╗ ████║██╔══██╗╚══██╔══╝██╔════╝██║   ██║██║████╗ ████║
+██╔████╔██║███████║   ██║   █████╗  ██║   ██║██║██╔████╔██║
+██║╚██╔╝██║██╔══██║   ██║   ██╔══╝  ╚██╗ ██╔╝██║██║╚██╔╝██║
+██║ ╚═╝ ██║██║  ██║   ██║   ███████╗ ╚████╔╝ ██║██║ ╚═╝ ██║
+╚═╝     ╚═╝╚═╝  ╚═╝   ╚═╝   ╚══════╝  ╚═══╝  ╚═╝╚═╝     ╚═╝
+                                                            ]],
+          -- stylua: ignore
+          ---@type snacks.dashboard.Item[]
+          keys = {
+            { icon = " ", key = "f", desc = "Find File", action = ":lua Snacks.dashboard.pick('files')" },
+            { icon = " ", key = "n", desc = "New File", action = ":ene | startinsert" },
+            { icon = " ", key = "g", desc = "Find Text", action = ":lua Snacks.dashboard.pick('live_grep')" },
+            { icon = " ", key = "r", desc = "Recent Files", action = ":lua Snacks.dashboard.pick('oldfiles')" },
+            { icon = " ", key = "c", desc = "Config", action = ":lua Snacks.dashboard.pick('files', {cwd = vim.fn.stdpath('config')})" },
+            { icon = " ", key = "s", desc = "Restore Session", section = "session" },
+            { icon = "󰒲 ", key = "l", desc = "Lazy", action = ":Lazy" },
+            { icon = " ", key = "q", desc = "Quit", action = ":qa" },
+          },
+				},
+			},
+			bigfile = { enabled = true },
+			bufdelete = { enabled = true },
+			notifier = { enabled = true },
+			quickfile = { enabled = true },
+			statuscolumn = { enabled = true },
+			words = { enabled = true },
+		},
 	},
 }
